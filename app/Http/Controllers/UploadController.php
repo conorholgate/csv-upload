2ce75d79699cdbb585324657fc8198026310ffc8
Conor Holgate <conor.ianholgate@gmail.com>
2023-04-08 18:54:49 +0100

    initial commit

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UploadController extends Controller
+{
+    public function upload(Request $request)
+    {
+
+        $file = $request->files->get('file');
+
+        // Open the CSV file and read the data
+        $csv = array_map('str_getcsv', file($file));
+        $data = array_slice($csv, 1);
+
+        $people = [];
+        // Loop through the CSV data and create records for each person
+        foreach ($data as $row) {
+            // Split the name into title and last name
+            $name_parts = explode(' ', $row[0]);
+            $last_name = array_pop($name_parts);
+            $title = implode(' ', $name_parts);
+
+            // Check if the title contains "and" or "&"
+            if (strpos($title, ' and ') !== false || strpos($title, ' & ') !== false) {
+                // If the title contains "and" or "&", create records for each person
+                $names = preg_split('/\s+and\s+|\s+&\s+/', $title);
+                foreach ($names as $name) {
+
+                    $person = $this->createPersonRecord($name, $last_name);
+                    $people[] = $person;
+                }
+            } else {
+                // If the title does not contain "and" or "&", create a record for the person
+                $person = $this->createPersonRecord($title, $last_name);
+                $people[] = $person;
+            }
+        }
+
+        // Return people array
+        return response()->json(['data' => $people]);
+    }
+    private function createPersonRecord($title, $last_name) {
+        // Split the title into title and initial (if applicable)
+        $title_parts = explode(' ', $title);
+        $initial = null;
+        $first_name = null;
+        if (count($title_parts) > 1) {
+            $first_name = array_pop($title_parts);
+            $initial = substr($first_name, 0, 1);
+        }
+        $title = implode(' ', $title_parts);
+    
+        // Create a new Person record
+        $person = [
+            'title' => $title,
+            'initial' => $initial,
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+        ];
+        return $person;
+    }
+}
+
+
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="flex items-center justify-center min-h-screen">
+        <div class="flex flex-col items-center">
+            <form @submit.prevent="uploadFile" enctype="multipart/form-data">
+                <input type="file" ref="fileInput" accept=".csv" />
+                <button type="submit">Upload CSV</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import { useForm } from "@inertiajs/vue3";
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const fileInput = ref(null);
+
+        const uploadFile = () => {
+            const file = fileInput.value.files[0];
+            const form = useForm({
+                file,
+            });
+            form.post(route("file.upload"), {
+                onSuccess: () => form.reset(),
+                onError: () => {
+                    console.log("error");
+                },
+            });
+        };
+
+        return {
+            uploadFile,
+            fileInput,
+        };
+    },
+};
+</script>
+
+<style>
+.bg-dots-darker {
+    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
+}
+@media (prefers-color-scheme: dark) {
+    .dark\:bg-dots-lighter {
+        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
+    }
+}
+</style>
